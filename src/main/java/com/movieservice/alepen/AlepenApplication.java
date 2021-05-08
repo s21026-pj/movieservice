9dd11bafad4ea1c5b4de88cfacc96b5f62971f15
s21026-pj <56227325+s21026-pj@users.noreply.github.com>
2021-05-08 16:25:41 +0200

    Initial commit

---
+++ src/main/java/com/movieservice/alepen/AlepenApplication.java
@@ -0,0 +1,13 @@
+package com.movieservice.alepen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlepenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlepenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movieservice/alepen/AlepenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieservice.alepen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlepenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
